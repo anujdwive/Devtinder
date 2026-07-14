e87cbd38bd3ebc2e2d56395d6c076ef7527ac596
Anuj Dwivedi <anujdwivedi0705@gmail.com>
2026-07-14 10:02:20 +0530

    add profile route

--- devtinder-frontend/src/App.jsx
+++ devtinder-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/screen/Login";
 import Layout from "./components/layout/Layout";
 import Dashboard from "./components/screen/Dashboard";
+import Profile from "./components/screen/Profile";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<Layout />}>
           <Route index element={<Navigate to='/login' replace />} />
           <Route path='login' element={<Login />} />
+          <Route path='profile' element={<Profile />} />
           <Route path='dashboard' element={<Dashboard />} />
         </Route>
       </Routes>
