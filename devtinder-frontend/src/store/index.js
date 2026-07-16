352f16b6cb06bb838ccfe2090cb6e5e4000273e2
Anuj Dwivedi <anujdwivedi0705@gmail.com>
2026-07-16 12:55:39 +0530

    add connection reducer

--- devtinder-frontend/src/store/index.js
+++ devtinder-frontend/src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import feedReducer from "./slices/feedSlice";
+import connectionReducer from "./slices/connectionSlice";
 
 const store = configureStore({
   reducer: {
     user: authReducer,
     feedUser: feedReducer,
+    connection: connectionReducer,
   },
 });
 
